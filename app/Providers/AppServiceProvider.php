690287b1c93915f342930b36a89d6fb37659aca9
tlotli <tlotlimosalo@gmail.com>
2019-04-05 13:44:57 +0200

    Uncommented Varaibles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,31 +18,31 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 
-//    	$set_date_flag = SetDateFlag::first();
-//
-//        $overdue_visitations = DB::select("
-//            SELECT DATEDIFF(CURRENT_DATE ,v.date_visited ) AS date_visited , sites.name AS site_name , sites.id AS site_id
-//			FROM visitations v
-//			INNER JOIN sites
-//			ON sites.id = v.site_id
-//			WHERE v.date_visited =
-//			(SELECT MAX(visitations.date_visited) FROM visitations WHERE v.site_id = visitations.site_id )
-//			AND DATEDIFF(CURRENT_DATE ,v.date_visited )  > $set_date_flag->date_flag
-//			LIMIT 15
-//        ");
-//
-//	    $overdue_count = DB::select("
-//	                    SELECT COUNT(v.id) AS overdue_count
-//						FROM visitations v
-//						INNER JOIN sites
-//						ON sites.id = v.site_id
-//						WHERE v.date_visited =
-//						(SELECT MAX(visitations.date_visited) FROM visitations WHERE v.site_id = visitations.site_id )
-//						AND DATEDIFF(CURRENT_DATE ,v.date_visited )  > $set_date_flag->date_flag
-//	    ");
-//
-//        View::share('overdue_visitations' , $overdue_visitations);
-//        View::share('overdue_count' , $overdue_count);
+    	$set_date_flag = SetDateFlag::first();
+
+        $overdue_visitations = DB::select("
+            SELECT DATEDIFF(CURRENT_DATE ,v.date_visited ) AS date_visited , sites.name AS site_name , sites.id AS site_id
+			FROM visitations v
+			INNER JOIN sites
+			ON sites.id = v.site_id
+			WHERE v.date_visited =
+			(SELECT MAX(visitations.date_visited) FROM visitations WHERE v.site_id = visitations.site_id )
+			AND DATEDIFF(CURRENT_DATE ,v.date_visited )  > $set_date_flag->date_flag
+			LIMIT 15
+        ");
+
+	    $overdue_count = DB::select("
+	                    SELECT COUNT(v.id) AS overdue_count
+						FROM visitations v
+						INNER JOIN sites
+						ON sites.id = v.site_id
+						WHERE v.date_visited =
+						(SELECT MAX(visitations.date_visited) FROM visitations WHERE v.site_id = visitations.site_id )
+						AND DATEDIFF(CURRENT_DATE ,v.date_visited )  > $set_date_flag->date_flag
+	    ");
+
+        View::share('overdue_visitations' , $overdue_visitations);
+        View::share('overdue_count' , $overdue_count);
 
     }
 
