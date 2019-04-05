4c71cf9fd573b208c182a222f036a3bed2c9a4e9
tlotli <tlotlimosalo@gmail.com>
2019-04-05 07:28:23 +0200

    took out notifications with comments

--- resources/views/layouts/top_menu_nav.blade.php
+++ resources/views/layouts/top_menu_nav.blade.php
@@ -36,20 +36,20 @@
                         <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; height: 229px;"><div class="streamline message-nicescroll-bar" style="overflow: hidden; width: auto; height: 229px;">
                                 <hr class="light-grey-hr ma-0">
 
-                                @foreach($overdue_visitations as $o)
-                                    <div class="sl-item">
-                                        <a href="javascript:void(0)">
+                                {{--@foreach($overdue_visitations as $o)--}}
+                                    {{--<div class="sl-item">--}}
+                                        {{--<a href="javascript:void(0)">--}}
 
-                                            <div class="sl-content">
-                                                <span class="inline-block capitalize-font  pull-left truncate head-notifications txt-danger"><a
-                                                            href="{{route('visitations_list' , ['id' => $o->site_id])}}">{{$o->site_name}}</a></span>
-                                                <span class="inline-block font-11 pull-right notifications-time">{{$o->date_visited . ' days ago'}}</span>
-                                                <div class="clearfix"></div>
-                                            </div>
-                                            
-                                        </a>
-                                    </div>
-                                @endforeach
+                                            {{--<div class="sl-content">--}}
+                                                {{--<span class="inline-block capitalize-font  pull-left truncate head-notifications txt-danger"><a--}}
+                                                            {{--href="{{route('visitations_list' , ['id' => $o->site_id])}}">{{$o->site_name}}</a></span>--}}
+                                                {{--<span class="inline-block font-11 pull-right notifications-time">{{$o->date_visited . ' days ago'}}</span>--}}
+                                                {{--<div class="clearfix"></div>--}}
+                                            {{--</div>--}}
+                                            {{----}}
+                                        {{--</a>--}}
+                                    {{--</div>--}}
+                                {{--@endforeach--}}
 
                             </div><div class="slimScrollBar" style="background: rgb(135, 135, 135) none repeat scroll 0% 0%; width: 4px; position: absolute; top: 0px; opacity: 0.4; display: block; border-radius: 0px; z-index: 99; right: 1px;"></div><div class="slimScrollRail" style="width: 4px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51) none repeat scroll 0% 0%; opacity: 0.2; z-index: 90; right: 1px;"></div></div>
                     </li>
